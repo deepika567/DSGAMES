d59860bfc31bfb9cde929ead33e4b52a608b3d47
Deepika Sharma <deepuss760@gmail.com>
2020-06-20 23:48:52 +0530

    Update snake.js

--- snake/snake.js
+++ snake/snake.js
@@ -1,7 +1,23 @@
 function init(){
     var canvas=document.getElementById('mycanvas');
     W=H=canvas.width=canvas.height=1000;
+    pen=canvas.getcontext('2d');
+    snake={
+        init_len=5,
+        color:"blue",
+        cells:[],
+        direction:"right",
+   
+        createSnake:function(){
+            for(var i=this.init_len;i>=0;i--){
+                
 
+            }
+
+        }
+
+
+    };
 
 }
 function draw(){

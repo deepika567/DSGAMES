3b91778ac6a4eb972b7074bebc4a1f2397d51074
Deepika Sharma <deepuss760@gmail.com>
2020-06-20 23:38:01 +0530

    bacground image
    commit to bg image

---
+++ snake/snake.js
@@ -0,0 +1,17 @@
+function init(){
+    var canvas=document.getElementById('mycanvas');
+    W=H=canvas.width=canvas.height=1000;
+
+
+}
+function draw(){
+
+}
+function update(){
+
+}
+function gameloop(){
+
+}
+init();
+ var f= setInterval(gameloop,100);

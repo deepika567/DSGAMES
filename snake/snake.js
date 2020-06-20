985be17fab5b72c7ecb51ef234bf44425dbd42c6
Deepika Sharma <deepuss760@gmail.com>
2020-06-21 00:15:30 +0530

    Update snake.js

--- snake/snake.js
+++ snake/snake.js
@@ -1,29 +1,28 @@
 function init(){
-     canvas=document.getElementById('mycanvas');
-    W= H= canvas.width=canvas.height=1000;
-    pen=canvas.getcontext('2d');
+    canvas = document.getElementById('mycanvas');
+	W = H = canvas.width = canvas.height = 1000;
+	pen = canvas.getContext('2d');
     cs=50;
 
-    snake={
-        init_len=5,
-        color:"blue",
-        cells:[],
-        direction:"right",
-   
-        createSnake:function(){
-            for(var i=this.init_len;i>=0;i--){
-                this.cells.push({x:i,y:0});
+    snake = {
+		init_len:5,
+		color:"blue",
+		cells:[],
+		direction:"right",
 
+
+		createSnake:function(){
+			for(var i=this.init_len;i>0;i--){
+				this.cells.push({x:i,y:0});
+			}
+		},
+		drawSnake:function(){
+
+			for(var i=0;i<this.cells.length;i++){
+				pen.fillStyle = this.color;
+				pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-3,cs-3);
             }
-
-        },
-        drawSnake:function(){
-            pen.fillRect(this.cells[i].x,this.cells[i].y,cs,cs);
-
         }
-       
-
-
     };
     snake.createSnake();
 
